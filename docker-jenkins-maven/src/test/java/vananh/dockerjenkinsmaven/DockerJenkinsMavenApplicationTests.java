caf713aa8390daf9e8a2a10baa2a36203b7b3809
vananhit10391 <vananhit10391@gmail.com>
2021-04-12 10:27:40 +0700

    second commit

---
+++ docker-jenkins-maven/src/test/java/vananh/dockerjenkinsmaven/DockerJenkinsMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package vananh.dockerjenkinsmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJenkinsMavenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
